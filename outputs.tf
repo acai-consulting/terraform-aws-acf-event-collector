1505141216a32a60bf470147fc1366f9f92ef544
Michael Ullrich <michael.ullrich@nuvibit.com>
2024-05-15 20:45:50 +0200

    fix

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@ output "eventbus_arn" {
   description = "eventbus_arn"
   value       = aws_cloudwatch_event_bus.collector.arn
 }
+
+
+output "configuration_to_write" {
+  description = "HCL map to be stored in configuration map"
+  value = {
+    central_eventbus_arn = aws_cloudwatch_event_bus.collector.arn
+  } 
+}
+
